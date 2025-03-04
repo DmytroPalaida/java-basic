5348df5e2d72126549ad7ecbc02b35a7c7ac22c0
DmytroPalaida <mr.palayda@gmail.com>
2025-03-04 20:08:34 +0200

    modele1

---
+++ src/module_1/meet_a_stranger/main/MeetAStranger.java
@@ -0,0 +1,12 @@
+package module_1.meet_a_stranger.main;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        System.out.println("Hello, " + num);
+
+    }
+}
---
+++ src/module_1/meet_autocode/main/HelloAutocode.java
@@ -0,0 +1,8 @@
+package module_1.meet_autocode.main;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+
+    }
+}
