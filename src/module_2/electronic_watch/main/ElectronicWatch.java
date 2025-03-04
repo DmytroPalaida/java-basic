2b1601e9b2f610e9bc7729455fb20a8239d8dd24
DmytroPalaida <mr.palayda@gmail.com>
2025-03-04 20:19:05 +0200

    modele2

---
+++ src/module_2/electronic_watch/main/ElectronicWatch.java
@@ -0,0 +1,33 @@
+package module_2.electronic_watch.main;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+        /*
+        ZonedDateTime nowZoned = ZonedDateTime.now();
+        Instant midnight = nowZoned.toLocalDate().atStartOfDay(nowZoned.getZone()).toInstant();
+        Instant currentTime = midnight.plus(seconds, ChronoUnit.SECONDS);
+        Duration duration = Duration.between(midnight, currentTime);
+        String hms = String.format("%d:%02d:%02d",
+                duration.toHours() % 24,
+                duration.toMinutesPart(),
+                duration.toSecondsPart());
+        System.out.println(hms);
+*/
+
+        int hours = seconds / 60 / 60;
+        int minutes = (seconds / 60) - (hours * 60);
+        int second = seconds % 60;
+
+        String hms = String.format("%01d:%02d:%02d", hours%24, minutes, second);
+        System.out.println(hms);
+
+    }
+}
